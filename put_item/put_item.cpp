63baec2fd22c98231dbb64254c52867daf1b695f
famous05 <famous.osarobo@gmail.com>
2021-04-06 15:43:47 -0700

    Minor changes

--- put_item/put_item.cpp
+++ put_item/put_item.cpp
@@ -1,18 +1,5 @@
- 
-
-
 /*
-Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-This file is licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License. A copy of
-the License is located at
-
-http://aws.amazon.com/apache2.0/
-
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
+Copyright 2021 Osarobo Famous Okhuahesogie, famous.osarobo@gmail.com
 */
 
 #include <aws/core/Aws.h>
@@ -27,11 +14,7 @@ specific language governing permissions and limitations under the License.
 /**
 * Put an item in a DynamoDB table.
 *
-* Takes the name of the table, a name (primary key value) and a greeting
-* (associated with the key value).
-*
-* This code expects that you have AWS credentials set up per:
-* http://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/credentials.html
+* Takes the name of the table, primary key "id" and associated key value pairs
 */
 int main(int argc, char** argv)
 {
