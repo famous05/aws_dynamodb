4301e5c1ed2d33ceca6a2e5568a2ea2913d55d4c
famous05 <famous.osarobo@gmail.com>
2021-04-14 15:59:37 -0700

    Added create_table.cpp

---
+++ create_table/create_table.cpp
@@ -0,0 +1,90 @@
+/*
+
+ (c) 2021 Osarobo Famous Okhuahesogie
+
+*/
+#include <aws/core/Aws.h>
+#include <aws/core/utils/Outcome.h> 
+#include <aws/dynamodb/DynamoDBClient.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/CreateTableRequest.h>
+#include <aws/dynamodb/model/KeySchemaElement.h>
+#include <aws/dynamodb/model/ProvisionedThroughput.h>
+#include <aws/dynamodb/model/ScalarAttributeType.h>
+#include <iostream>
+
+
+/**
+* Create an Amazon DynamoDB table.
+*/
+
+int main(int argc, char** argv)
+{
+    const std::string USAGE = "\n" \
+        "Usage:\n"
+        "    create_table <region>\n\n"
+        "Where:\n"
+        "    region- optional region\n\n"
+        "Example:\n"
+        "    create_table eu-west-1\n";
+
+    if (argc < 2)
+    {
+        std::cout << USAGE;
+        return 1;
+    }
+
+    Aws::SDKOptions options;
+
+    Aws::InitAPI(options);
+    {
+    	// Create table *******************************************************
+        const Aws::String region(argv[1]);
+        //const Aws::String region(argc > 2 ? argv[2] : "");
+        
+        const Aws::String table {"Teams"};
+
+        Aws::Client::ClientConfiguration clientConfig;
+        if (!region.empty())
+            clientConfig.region = region;
+        Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfig);
+
+        std::cout << "Creating table " << table <<
+            " with a simple primary key: \"id\"" << std::endl;
+
+        Aws::DynamoDB::Model::CreateTableRequest req;
+
+        Aws::DynamoDB::Model::AttributeDefinition haskKey;
+        haskKey.SetAttributeName("id");
+        haskKey.SetAttributeType(Aws::DynamoDB::Model::ScalarAttributeType::S);
+        req.AddAttributeDefinitions(haskKey);
+
+        Aws::DynamoDB::Model::KeySchemaElement keyscelt;
+        keyscelt.WithAttributeName("id").WithKeyType(Aws::DynamoDB::Model::KeyType::HASH);
+        req.AddKeySchema(keyscelt);
+
+        Aws::DynamoDB::Model::ProvisionedThroughput thruput;
+        thruput.WithReadCapacityUnits(5).WithWriteCapacityUnits(5);
+        req.SetProvisionedThroughput(thruput);
+
+        req.SetTableName(table);
+        
+      
+        const Aws::DynamoDB::Model::CreateTableOutcome& result = dynamoClient.CreateTable(req);
+        if (result.IsSuccess())
+        {
+            std::cout << "Table \"" << result.GetResult().GetTableDescription().GetTableName() <<
+                " created!" << std::endl;
+        }
+        else
+        {
+            std::cout << "Failed to create table: " << result.GetError().GetMessage();
+        }
+        // *****************************************************************************
+        
+        
+    }
+    Aws::ShutdownAPI(options);
+    return 0;
+}
+
